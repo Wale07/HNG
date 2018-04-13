df9aa714074c1403e427d45f770aee9f94f7a8df
unknown <Wale_odufuwa>
2018-04-13 23:25:01 +0100

    first commit

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+ <title> My Figma Page </title>
+ <link rel = "stylesheet" type = "text/css" href = "index.css" />
+</head>
+<body>
+  <h1> HNG INTERNSHIP</h1>
+  <p> Software Developer </p>
+ <?php
+    $time = time();
+    $actual_time = date('F jS Y h:i a' );
+
+     ?>
+   <p><?php echo 'Date/Time: '.$actual_time;?></p>
+</body>
+</html>
